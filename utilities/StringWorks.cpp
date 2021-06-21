fc621531de0b3196ff2f1f3a86d8e519495b5ca4
c0dezer019 <c0dezer019@bblanke nship.me>
2021-06-21 18:21:19 -0500

    function to convert array back to string.

--- utilities/StringWorks.cpp
+++ utilities/StringWorks.cpp
@@ -5,8 +5,20 @@ using namespace std;
 char* StrToArr(string str)
 {
     int n = str.length();
-    char* arr = new char[n + 1];
+    int& NRef = n;
+    char* arr = new char[NRef + 1];
     strcpy(arr, str.c_str());
 
     return arr;
+}
+
+string ArrToStr(char* arr, int size)
+{
+    int i;
+    string str = "";
+
+    for (i = 0; i < size; i++) 
+        str = str + arr[i];
+
+    return str;
 }
