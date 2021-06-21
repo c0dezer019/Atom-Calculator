4c63876d2047d29a0077053f723c02f61bb56337
c0dezer019 <c0dezer019@zohomail.com>
2021-06-20 23:36:16 -0500

    Add project files.

---
+++ AtomCalculator.cpp
@@ -0,0 +1,12 @@
+﻿// AtomCalculator.cpp : Defines the entry point for the application.
+//
+
+#include "AtomCalculator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
